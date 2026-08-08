1a77cd3fc8b435756658ef4f4e07f02247b73a34
AreebaDevCodes <mniareeba04@gmail.com>
2026-08-08 08:08:43 +0100

    Build initial loader and navbar

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/* =========================
+   Midnight Bloom Café
+   Loader Functionality
+========================= */
+
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(function () {
+
+        loader.classList.add("hide");
+
+    }, 1000);
+
+});
